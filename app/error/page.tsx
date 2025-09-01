5e2ccfc53513ab3d00b6812246d8fddb5d13ad75
JoelVCrasta <joelcrasta03@proton.me>
2025-09-01 22:40:44 +0530

    added not found page

--- app/error/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const ErrorPage = () => {
-  return <div>Somthing went wrong</div>
-}
-
-export default ErrorPage
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="flex h-screen items-center justify-center flex-col">
+      <h1 className="text-4xl font-bold my-2">404</h1>
+      <p className="text-gray-600">This page doesn’t exist :P</p>
+    </div>
+  )
+}
+
+export default NotFound

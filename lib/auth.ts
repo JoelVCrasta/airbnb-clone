e0610886f813b530753eeccc1839e1e9c2e95e26
JoelVCrasta <joelcrasta03@proton.me>
2025-09-01 21:44:47 +0530

    first commit

---
+++ lib/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import prisma from "@/db"
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "mongodb",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+    minPasswordLength: 6,
+    maxPasswordLength: 100,
+  },
+  socialProviders: {
+    google: {
+      prompt: "select_account consent",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      accessType: "offline",
+    },
+  },
+})

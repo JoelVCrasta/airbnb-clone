57f2d6b6cd81b68fa5d9ce04db8b7092224e9c05
JoelVCrasta <joelcrasta03@proton.me>
2025-09-03 22:41:55 +0530

    addded schema for listings and reservations

---
+++ utils/types.ts
@@ -0,0 +1,82 @@
+export interface User {
+  id: string
+  name: string
+  email: string
+  emailVerified: boolean
+  image?: string
+  createdAt: Date
+  updatedAt: Date
+  sessions?: Session[]
+  accounts?: Account[]
+  listings?: Listing[]
+  reservations?: Reservation[]
+}
+
+export interface Session {
+  id: string
+  expiresAt: Date
+  token: string
+  createdAt: Date
+  updatedAt: Date
+  ipAddress?: string
+  userAgent?: string
+  userId: string
+  user?: User
+}
+
+export interface Account {
+  id: string
+  accountId: string
+  providerId: string
+  userId: string
+  user?: User
+  accessToken?: string
+  refreshToken?: string
+  idToken?: string
+  accessTokenExpiresAt?: Date
+  refreshTokenExpiresAt?: Date
+  scope?: string
+  password?: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface Verification {
+  id: string
+  identifier: string
+  value: string
+  expiresAt: Date
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export interface Listing {
+  id: string
+  userId: string
+  user?: User
+  title: string
+  description: string
+  imageUrls: string[]
+  category: string
+  roomCount: number
+  bathroomCount: number
+  guestCount: number
+  price: number
+  location: string
+  reservations?: Reservation[]
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface Reservation {
+  id: string
+  userId: string
+  user?: User
+  listingId: string
+  listing?: Listing
+  checkIn: Date
+  checkOut: Date
+  totalPrice: number
+  createdAt: Date
+  updatedAt: Date
+}

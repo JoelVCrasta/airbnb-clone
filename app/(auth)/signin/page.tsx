13479cf33632cb5a6bfa04252186e8318e00e462
JoelVCrasta <joelcrasta03@proton.me>
2025-09-03 22:41:02 +0530

    added server session check

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,6 +1,13 @@
 import SignIn from "@/components/auth/signin"
+import { getSession } from "@/lib/get_session"
+import { redirect } from "next/navigation"
 
-const SignInPage = () => {
+const SignInPage = async () => {
+  const session = await getSession()
+  if (session) {
+    redirect("/")
+  }
+
   return <SignIn />
 }
 
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,6 +1,13 @@
 import SignUp from "@/components/auth/signup"
+import { getSession } from "@/lib/get_session"
+import { redirect } from "next/navigation"
 
-const SignUpPage = () => {
+const SignUpPage = async () => {
+  const session = await getSession()
+  if (session) {
+    redirect("/")
+  }
+
   return <SignUp />
 }
 

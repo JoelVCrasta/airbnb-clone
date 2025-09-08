6c5fbdd382dbf2c59474ac901ef660e4eb49eb9b
JoelVCrasta <joelcrasta03@proton.me>
2025-09-08 14:53:28 +0530

    fixed overflow issue in auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      {children}
-    </div>
+    <div className="flex items-center justify-center h-screen">{children}</div>
   )
 }
 
